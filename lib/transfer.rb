4762ca5b60070ae8d740dc216e6dfd5c368ca83c
Nancy Jackson <nbjackson23@gmail.com>
2020-09-04 17:45:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
   
-  attr_accessor :sender :receiver :amount :status
+  attr_accessor :sender, :receiver, :amount, :status
   
-  def initialize ()
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
 end
