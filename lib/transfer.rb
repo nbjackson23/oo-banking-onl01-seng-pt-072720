852980b2eb751aa7054a07e675c0d35144e7419b
Nancy Jackson <nbjackson23@gmail.com>
2020-09-04 18:11:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
     else
       self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
