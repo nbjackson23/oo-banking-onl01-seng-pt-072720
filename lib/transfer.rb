e4cc9cb724690e95332f9f1ee208bd0340bde22e
Nancy Jackson <nbjackson23@gmail.com>
2020-09-04 18:25:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,9 @@ class Transfer
   end
   
   def execute_transaction
+    binding.pry
     if @sender.balance > @amount && @status == "pending"
-      binding.pry
+      
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
